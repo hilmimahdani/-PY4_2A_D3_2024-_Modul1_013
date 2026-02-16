2dfdf2a9326173d679c9d7491a76eb991378c5cf
hilmimahdani <hilmimahdanib@gmail.com>
2026-02-16 12:59:25 +0700

    Modul 2 Task 3: Persistent History Logger(HOTS)

--- lib/features/logbook/counter_controller.dart
+++ lib/features/logbook/counter_controller.dart
@@ -1,43 +1,66 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class CounterController {
   int _counter = 0; 
   int _step = 1;
-
-  final List<String> _history = [];
+  List<String> _history = [];
 
   int get value => _counter; 
   int get step => _step;
-
   List<String> get history => _history;
+
+  String _makeKeyCounter(String username) => 'counter_value_$username';
+  String _makeKeyHistory(String username) => 'history_list_$username';
+
+  Future<void> loadData(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    
+    _counter = prefs.getInt(_makeKeyCounter(username)) ?? 0;
+    
+    _history = prefs.getStringList(_makeKeyHistory(username)) ?? [];
+  }
+
+  Future<void> _saveData(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_makeKeyCounter(username), _counter);
+    await prefs.setStringList(_makeKeyHistory(username), _history);
+  }
+
 
   void updateStep(int newStep) {
     _step = newStep;
   }
 
-  void _addToHistory(String action) {
+  void _addToHistory(String action, String username) {
     DateTime now = DateTime.now();
     String timestamp = "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}";
     
-    _history.insert(0, "$action pada jam $timestamp");
+    String logMessage = "Pengguna $username $action pada jam $timestamp";
+
+    _history.insert(0, logMessage);
 
     if (_history.length > 5) {
       _history.removeLast();
     }
   }
 
-  void increment() {
+  Future<void> increment(String username) async {
     _counter += _step;
-    _addToHistory("Anda menambah nilai sebesar $_step");
+    _addToHistory("menambah +$_step", username);
+    await _saveData(username);
   } 
 
-  void decrement() { 
+  Future<void> decrement(String username) async { 
     _counter -= _step;
-    _addToHistory("Anda mengurangi nilai sebesar $_step");
+    _addToHistory("mengurangi -$_step", username);
+    await _saveData(username);
   }
 
-  void reset() {
+  Future<void> reset(String username) async {
     _counter = 0;
     _step = 1;
-    _addToHistory("Anda melakukan reset");
+    _addToHistory("melakukan reset", username);
+    await _saveData(username);
   }
 
 }
--- lib/features/logbook/counter_view.dart
+++ lib/features/logbook/counter_view.dart
@@ -17,6 +17,52 @@ class _CounterViewState extends State<CounterView> {
 
   final CounterController _controller = CounterController();
 
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadInitialData();
+  }
+
+  void _loadInitialData() async {
+    await _controller.loadData(widget.username); 
+    if (mounted){
+      setState(() {
+        _isLoading = false; 
+      });
+    }
+  }
+
+  void _handleLogout() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Konfirmasi Logout"),
+          content: const Text("Apakah Anda yakin? Data yang belum disimpan mungkin akan hilang."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Batal"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OnboardingView()),
+                  (route) => false,
+                );
+              },
+              child: const Text("Ya, Keluar", style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,61 +72,97 @@ class _CounterViewState extends State<CounterView> {
       actions: [
         IconButton(
           icon: const Icon(Icons.logout),
-          onPressed: (){
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title : const Text("Konfirmasi Logout"),
-                  content: const Text ("Apakah Anda yakin? Data yang belum disimpan mungkin akan hilang."),
-                  actions: [
-                    //tombol batal
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(), 
-                      child: const Text("Batal"),
-                    ),
-
-                    //tombol logout
-                    TextButton(
-                      onPressed: () {
-                        
-                        Navigator.pop(context);
-
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => const OnboardingView()),
-                          (route) => false, 
-                        );
-                      },
-                      child: const Text("Ya, Keluar", style: TextStyle(color: Colors.red)),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
+          onPressed: _handleLogout,
         ),
-      
-      ]
-    ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      ],
+      ),
+
+
+      body: _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Padding (
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             children: [
-              Text ("Selamat Datang, ${widget.username}!"),
-              const SizedBox(height: 10),
-              const Text("Total Hitungan Anda:"),
+              const SizedBox(height: 20),
+              const Text("Total Hitungan Terakhir: "),
               Text(
                 '${_controller.value}',
-                style: Theme.of(context).textTheme.headlineLarge,
+                style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
               ),
+              const SizedBox(height: 20),
+            
+            //tombol kontrol
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                heroTag: "btnDec",
+                onPressed: () async {
+                  await _controller.decrement(widget.username);
+                  setState(() {});
+                },
+                backgroundColor: Colors.redAccent,
+                child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await _controller.reset(widget.username);
+                    setState(() {});
+                  },
+                  child: const Text("reset"),
+                ),
+                const SizedBox(width: 20),
+                FloatingActionButton(
+                  heroTag: "btnInc",
+                  onPressed: () async {
+                    await _controller.increment(widget.username);
+                    setState(() {});
+                  },
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.add),
+                ),
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() => _controller.increment()),
-          child: const Icon(Icons.add),
-        ),
+
+          const SizedBox(height: 30),
+          const Divider(),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Riwayat Aktivitas:",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+          ),
+          const SizedBox(height: 10),
+
+          //List Riwayat
+          Expanded(
+            child: _controller.history.isEmpty
+                ? const Center(child: Text("Belum ada aktivitas."))
+                : ListView.builder(
+                    itemCount: _controller.history.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        child: ListTile(
+                          leading: const Icon(Icons.history, size: 18),
+                          title: Text(
+                            _controller.history[index],
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
     );
   }
 }
