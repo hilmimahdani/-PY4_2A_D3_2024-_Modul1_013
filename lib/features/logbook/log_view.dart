bf54ac3179d3401689a35a5f8bed9bb16d516c3b
hilmimahdani <hilmimahdanib@gmail.com>
2026-02-23 11:50:41 +0700

    Task 2,3,4 done

---
+++ lib/features/logbook/log_view.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:logbook_app_001/features/logbook/log_controller.dart';
+import 'package:logbook_app_001/features/logbook/models/log_model.dart';
+
+// Di file log_view.dart
+class LogView extends StatefulWidget {
+  final String? username; 
+  const LogView({super.key, this.username}); 
+
+  @override
+  State<LogView> createState() => _LogViewState();
+}
+
+
+class _LogViewState extends State<LogView> {
+  late LogController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = LogController(widget.username ?? "default_user"); 
+  } 
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  void _showAddLogDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Tambah Catatan Baru"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(hintText: "Judul Catatan"),
+            ),
+            TextField(
+              controller: _contentController,
+              decoration: const InputDecoration(hintText: "Isi Deskripsi"),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _titleController.clear();
+              _contentController.clear();
+              Navigator.pop(context);
+            },
+            child: const Text("Batal"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _controller.addLog(
+                _titleController.text,
+                _contentController.text,
+              );
+              _titleController.clear();
+              _contentController.clear();
+              Navigator.pop(context);
+              setState(() {}); 
+            },
+            child: const Text("Simpan"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showEditLogDialog(int index, LogModel log) {
+    _titleController.text = log.title;
+    _contentController.text = log.description;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Edit Catatan"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(labelText: "Judul"),
+            ),
+            TextField(
+              controller: _contentController,
+              decoration: const InputDecoration(labelText: "Deskripsi"),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              _titleController.clear();
+              _contentController.clear();
+              Navigator.pop(context);
+            },
+            child: const Text("Batal"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _controller.updateLog(
+                index,
+                _titleController.text,
+                _contentController.text,
+              );
+              _titleController.clear();
+              _contentController.clear();
+              Navigator.pop(context);
+              setState(() {}); 
+            },
+            child: const Text("Update"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Log Notes")),
+      
+      body: ValueListenableBuilder<List<LogModel>>(
+        valueListenable: _controller.logsNotifier,
+        builder: (context, currentLogs, child) {
+          if (currentLogs.isEmpty) {
+            return const Center(child: Text("Belum ada catatan."));
+          }
+
+          return ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: currentLogs.length,
+            separatorBuilder: (context, index) => const SizedBox (height: 8),
+            itemBuilder: (context, index) {
+              final log = currentLogs[index];
+              return Card(
+                child: ListTile(
+                  leading: const Icon(Icons.note),
+                  title: Text(log.title),
+                  subtitle: Text(log.description),
+                  trailing: Wrap(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue),
+                        onPressed: () => _showEditLogDialog(index, log),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          setState(() => _controller.removeLog(index));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddLogDialog,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+}
+
